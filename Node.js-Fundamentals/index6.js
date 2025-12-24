684ba69b16c2d8a8cf5a5cd03b6c3134b772712f
Rushikesh Gaware <rushigaware1020@gmail.com>
2025-12-24 23:22:42 +0530

    operating system

---
+++ Node.js-Fundamentals/index6.js
@@ -0,0 +1,18 @@
+// const fs = require("fs")
+// const https = require("https")
+
+// const PORT = 3000
+
+// const options = {
+//   key: fs.readFileSync("./server.key"),
+//   cert: fs.readFileSync("./server.cert"),
+// };
+
+// const server = https.createServer(options, (req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" })
+//   res.end("HTTPS server is up and running");
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`HTTPS server running on https://localhost:${PORT}`)
+// });
---
+++ Node.js-Fundamentals/index7.js
@@ -0,0 +1,6 @@
+//operating system
+
+const os =require("os")
+console.log(`OS Platform: ${os.platform()}`);
+console.log("os type",os.type());
+console.log("os userInformation",os.userInfo());
