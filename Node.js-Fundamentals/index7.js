e37daae8868a7aac8b7d22520c6377f23643f678
Rushikesh Gaware <rushigaware1020@gmail.com>
2025-12-31 00:09:02 +0530

    nodejs add new file

--- Node.js-Fundamentals/index7.js
+++ Node.js-Fundamentals/index7.js
@@ -4,3 +4,8 @@ const os =require("os")
 console.log(`OS Platform: ${os.platform()}`);
 console.log("os type",os.type());
 console.log("os userInformation",os.userInfo());
+
+
+const path = require('path');
+const filename = path.basename('/users/91913/file.txt');
+console.log(filename);
